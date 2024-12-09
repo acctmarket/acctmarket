cee9ba803352ecac0966cc9776f29599c222a448
Gentility01 <mastergentility5@gmail.com>
2024-12-09 13:37:20 +0100

    added entry point

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,7 +7,7 @@ from .base import DATABASES, INSTALLED_APPS, REDIS_URL, env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["acctmarket.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
