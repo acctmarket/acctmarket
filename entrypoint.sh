34318caf28dc07343d4f7efd7b47975e697f31f2
Victor Isaac <tamsvictorisaac@gmail.com>
2024-12-10 16:33:12 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Your series of commands
-python manage.py migrate
+# python manage.py migrate
 # exec /usr/local/bin/gunicorn config.asgi --bind 0.0.0.0:5000 --chdir=/app -k uvicorn.workers.UvicornWorker
 exec python manage.py runserver 0.0.0.0:5000
