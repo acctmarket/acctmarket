42d906347dd259a923532d0aeaebeafe2782986e
Gentility01 <mastergentility5@gmail.com>
2024-12-28 16:09:35 +0100

    fixed ckeditor error

--- config/urls.py
+++ config/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
         )
     ),
 
-    # path("ckeditor/", include("ckeditor_uploader.urls")),
+    path("ckeditor/", include("ckeditor_uploader.urls")),
 
     # Media files
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
