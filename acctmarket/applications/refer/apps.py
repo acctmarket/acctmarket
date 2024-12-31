92ba29c87e3331fff2ec22185ef5ecbe4288c58b
Gentility01 <mastergentility5@gmail.com>
2024-12-31 20:36:46 +0100

    hot fixes

--- acctmarket/applications/refer/apps.py
+++ acctmarket/applications/refer/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class ReferConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "acctmarket.applications.refer"
+
+    def ready(self):
+        try:
+            import acctmarket.applications.refer.signals  # noqa F401
+        except ImportError:
+            pass
